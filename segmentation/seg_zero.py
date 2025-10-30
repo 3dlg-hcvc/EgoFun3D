7751b2ca1d83773f095b14637b3127b416217a6d
willipwk <p_w_kun@163.com>
2025-10-30 11:12:20 -0700

    debug kpts device

--- segmentation/seg_zero.py
+++ segmentation/seg_zero.py
@@ -171,6 +171,8 @@ class SegZero:
         print("convert to pixel coordinates...")
         kptsA, kptsB = self.feature_matching_model.to_pixel_coordinates(matches, H, W, H, W)
         print("finish conversion.")
+        kptsA = kptsA.cpu().numpy()
+        kptsB = kptsB.cpu().numpy()
         # Filter keypoints with part masks
         current_part_kpts = kptsA[current_part_mask[kptsA[:,1], kptsA[:,0]]]
         anchor_part_kpts = kptsB[anchor_part_mask[kptsB[:, 1], kptsB[:,0]]]
