8cf605fee8d97581e6295ea8a1dad7c4acdfe9af
willipwk <p_w_kun@163.com>
2025-11-06 09:18:44 -0800

    debug vlm config name

--- segmentation/seg_zero.py
+++ segmentation/seg_zero.py
@@ -170,7 +170,7 @@ def build_refseg_model(segmentation_config: dict) -> SegZero:
             segmentation_model_path=segmentation_config["segmentation_model_path"],
             segment_judge_config=segmentation_config["vlm_judge"],
             max_query=segmentation_config["max_query"],
-            device=segmentation_config.get("device", "cuda")
+            device=segmentation_config["device"]
         )
     else:
         raise ValueError(f"Unsupported segmentation model: {segmentation_config.model}")
