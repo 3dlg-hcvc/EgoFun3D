41c2399367a32fdd87f65431555e86131e4c65e7
willipwk <p_w_kun@163.com>
2025-11-13 10:38:19 -0800

    update exp save dir

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ def evaluate(input_modality: str, eval_dataset:BaseDataset, vlm_prompter: VLMPro
 def main(config: omegaconf.DictConfig):
     print("Start experiment:", config.name)
     exp_time = datetime.now().strftime("%Y%m%d-%H%M%S")
-    save_dir = f"{config.save_root_dir}/{config.name}_{exp_time}"
+    save_dir = f"{config.save_root_dir}/{config.name}/{exp_time}"
     print("Results will be saved to:", save_dir)
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
