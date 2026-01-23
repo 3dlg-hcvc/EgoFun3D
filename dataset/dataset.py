28dfefe09d9b604a233d2653dc02e415175e89a8
willipwk <p_w_kun@163.com>
2026-01-22 23:26:20 -0800

    update uniform dataset loader

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -4,6 +4,8 @@ import os
 import point_cloud_utils as pcu
 import open3d as o3d
 from PIL import Image as PILImage
+import gzip
+import pickle
 import glob
 
 
@@ -233,6 +235,148 @@ class iPhoneDataset(BaseDataset):
             }
         gt_annotations["relation"] = annotations[seg_id]["description"]
         return gt_annotations
+    
+
+class UniformDataset(BaseDataset):
+    def __init__(self, root_path: str, meta_file_path: str):
+        super().__init__(root_path, meta_file_path)
+        # Implement dataset loading logic here
+        self.data = []
+        with open(meta_file_path, "r") as f:
+            meta_info = json.load(f)
+        for video_dict in meta_info:
+            video_name = video_dict["video_name"]
+            # rgb
+            video_path = os.path.join(root_path, video_dict["video_path"])
+            video_frame_dir = os.path.join(root_path, video_dict["original_frame_dir"])
+            rgb_path_list = glob.glob(os.path.join(video_frame_dir, "*.jpg"))
+            rgb_path_list.sort()
+            rgb_list = []
+            for frame_path in rgb_path_list:
+                image = PILImage.open(frame_path)
+                image = image.convert("RGB")
+                rgb_list.append(image)
+            
+            # 2d masks
+            mask_path = os.path.join(root_path, video_dict["video_mask_path"])
+            with gzip.open(mask_path, "rb") as f:
+                mask_data = pickle.load(f)
+            receiver_mask_list = None
+            effector_mask_list = None
+            object_mask_list = None
+            receiver_name = None
+            effector_name = None
+            object_name = None
+            for mask_name in mask_data.keys():
+                if mask_data["mask_idx"] == 3:
+                    receiver_mask_list = mask_data["masks"]
+                    receiver_name = mask_name
+                elif mask_data["mask_idx"] == 4:
+                    effector_mask_list = mask_data["masks"]
+                    effector_name = mask_name
+                elif mask_data["mask_idx"] == 5:
+                    object_mask_list = mask_data["masks"]
+                    object_name = mask_name
+            
+            # 3d part point cloud
+            geometry_type = video_dict["geometry_type"]
+            geometry_path = os.path.join(root_path, video_dict["geometry_path"])
+            part_annotation_path = os.path.join(root_path, video_dict["part_annotation_path"])
+            function_instance_id = video_dict["function_instance_id"]
+            geometry_data = self.load_part_point_cloud(geometry_type, geometry_path, part_annotation_path, function_instance_id)
+
+            # articulation
+            receiver_articulation = None
+            effector_articulation = None
+            articulation_path = os.path.join(root_path, video_dict["articulation_path"])
+            with open(articulation_path, "r") as f:
+                articulation_data = json.load(f)
+            for role in ["receiver", "effector"]:
+                pid = geometry_data[role]["pid"]
+                for joint_data in articulation_data:
+                    if joint_data["pid"] == pid:
+                        if role == "receiver":
+                            receiver_articulation = joint_data
+                        elif role == "effector":
+                            effector_articulation = joint_data
+
+            # function
+            function_annotation_path = os.path.join(root_path, video_dict["function_annotation_path"])
+            with open(function_annotation_path, "r") as f:
+                function_data = json.load(f)
+            function_annotation = function_data[function_instance_id]
+            data_dict = {
+                "video_name": video_name,
+                "video_path": video_path,
+                "rgb_list": rgb_list,
+                "rgb_path_list": rgb_path_list,
+                "receiver_mask_list": receiver_mask_list,
+                "effector_mask_list": effector_mask_list,
+                "object_mask_list": object_mask_list,
+                "receiver_name": receiver_name,
+                "effector_name": effector_name,
+                "object_name": object_name,
+                "geometry_data": geometry_data,
+                "receiver_articulation": receiver_articulation,
+                "effector_articulation": effector_articulation,
+                "function_annotation": function_annotation
+            }
+            self.data.append(data_dict)
+
+    def load_part_point_cloud(self, geometry_type: str, geometry_path: str, part_annotation_path: str, function_instance_id: int) -> dict:
+        if geometry_type == "point cloud":
+            return self.load_point_cloud_data(geometry_path, part_annotation_path, function_instance_id)
+        elif geometry_type == "mesh":
+            return self.load_mesh_data(geometry_path, part_annotation_path, function_instance_id)
+        else:
+            raise ValueError(f"Unsupported geometry type: {geometry_type}")
+
+    def load_point_cloud_data(self, geometry_path: str, part_annotation_path: str, function_instance_id: int) -> dict:
+        full_pcd = pcu.load_mesh_v(geometry_path, np.float32)  # (N, 3)
+        with open(part_annotation_path, "r") as f:
+            annotations = json.load(f)
+        if function_instance_id not in annotations:
+            raise ValueError(f"Segment ID {function_instance_id} not found in annotations.")
+        geometry_annotations = {}
+        for role in ["receiver", "effector"]:
+            print(annotations[function_instance_id].keys())
+            part_name = annotations[function_instance_id][role]["label"]
+            part_indices = annotations[function_instance_id][role]["indices"]
+            pid = annotations[function_instance_id][role]["pid"]
+            if not part_indices:
+                raise ValueError(f"No indices found for role {role} in segment {function_instance_id}.")
+            part_pcd = full_pcd[part_indices]
+            geometry_annotations[role] = {
+                "part_pcd": part_pcd,
+                "pid": pid
+            }
+        geometry_annotations["relation"] = annotations[function_instance_id]["description"]
+        return geometry_annotations
+
+    def load_mesh_data(self, geometry_path: str, part_annotation_path: str, function_instance_id: int) -> dict:
+        full_mesh = o3d.io.read_triangle_mesh(geometry_path)
+        with open(part_annotation_path, "r") as f:
+            annotations = json.load(f)
+        if function_instance_id not in annotations.keys():
+            raise ValueError(f"Segment ID {function_instance_id} not found in annotations.")
+        geometry_annotations = {}
+
+        for role in ["receiver", "effector"]:
+            print(annotations[function_instance_id].keys())
+            part_name = annotations[function_instance_id][role]["label"]
+            part_indices = annotations[function_instance_id][role]["indices"]
+            pid = annotations[function_instance_id][role]["pid"]
+            if not part_indices:
+                raise ValueError(f"No indices found for role {role} in segment {function_instance_id}.")
+            part_mesh = full_mesh.select_by_index(part_indices)
+            part_pcd = part_mesh.sample_points_uniformly(number_of_points=10000)
+            part_pcd_np = np.asarray(part_pcd.points)
+            geometry_annotations[role] = {
+                "part_pcd": part_pcd_np,
+                "pid": pid
+            }
+        geometry_annotations["relation"] = annotations[function_instance_id]["description"]
+        return geometry_annotations
 
 
 def build_dataset(dataset_config: dict) -> BaseDataset:
@@ -244,6 +388,11 @@ def build_dataset(dataset_config: dict) -> BaseDataset:
         )
     elif dataset_name == "iPhone":
         return iPhoneDataset(
+            root_path=dataset_config["root_path"],
+            meta_file_path=dataset_config["meta_file"]
+        )
+    elif dataset_name == "Uniform":
+        return UniformDataset(
             root_path=dataset_config["root_path"],
             meta_file_path=dataset_config["meta_file"]
         )
