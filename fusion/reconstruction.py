deb253062227893fd391b9538c4e7cc923a81283
willipwk <p_w_kun@163.com>
2026-02-05 15:42:52 -0800

    add mapanything

--- fusion/reconstruction.py
+++ fusion/reconstruction.py
@@ -17,6 +17,11 @@ sys.path.append("third_party/Depth-Anything-3/")
 from depth_anything_3.api import DepthAnything3
 from depth_anything_3.specs import Prediction
 
+from mapanything.models import MapAnything
+from mapanything.utils.image import preprocess_inputs
+from mapanything.utils.hf_utils.viz import predictions_to_glb, image_mesh
+from mapanything.utils.geometry import depthmap_to_world_frame
+
 from utils.reconstruction_utils import depth2xyz
 
 from typing import List, Tuple, Dict
@@ -26,7 +31,7 @@ class BaseReconstruction:
     def __init__(self):
         pass
 
-    def reconstruct(self, video_frame_list: List[PILImage.Image], init_extrinsics: np.ndarray, intrinsics: np.ndarray = None, cam_pose_list: List[np.ndarray] = None, depth_frame_list: List[np.ndarray] = None) -> Dict[str, np.ndarray]:
+    def reconstruct(self, video_frame_list: List[PILImage.Image], init_extrinsics: np.ndarray, intrinsics: np.ndarray = None, cam_pose_list: np.ndarray = None, depth_frame_list: List[np.ndarray] = None) -> Dict[str, np.ndarray]:
         raise NotImplementedError
 
 
@@ -34,7 +39,7 @@ class NaiveReconstruction(BaseReconstruction):
     def __init__(self):
         pass
 
-    def reconstruct(self, video_frame_list: List[PILImage.Image], init_extrinsics: np.ndarray, intrinsics: np.ndarray, cam_pose_list: List[np.ndarray], depth_frame_list: List[np.ndarray]) -> Dict[str, np.ndarray]:
+    def reconstruct(self, video_frame_list: List[PILImage.Image], init_extrinsics: np.ndarray, intrinsics: np.ndarray, cam_pose_list: np.ndarray, depth_frame_list: List[np.ndarray]) -> Dict[str, np.ndarray]:
         point_map_list = []
         valid_mask_list = []
         cam2init = init_extrinsics @ np.linalg.inv(cam_pose_list[0])
@@ -48,7 +53,12 @@ class NaiveReconstruction(BaseReconstruction):
             point_map_list.append(points_map)
             valid_mask = np.ones_like(gt_depth, dtype=bool)
             valid_mask_list.append(valid_mask)
-        return {"rgb": video_frame_list, "intrinsics": intrinsics, "extrinsics": np.stack(cam_pose_list), "depth": np.stack(depth_frame_list), "points": np.stack(point_map_list), "points_mask": np.stack(valid_mask_list)}
+        return {"rgb": video_frame_list, 
+                "intrinsics": intrinsics, 
+                "extrinsics": np.stack(cam_pose_list), 
+                "depth": np.stack(depth_frame_list), 
+                "points": np.stack(point_map_list), 
+                "points_mask": np.stack(valid_mask_list)}
     
 
 class MoGeReconstruction(BaseReconstruction):
@@ -56,7 +66,7 @@ class MoGeReconstruction(BaseReconstruction):
         self.model = MoGeModel.from_pretrained(model_path).to(device)
         self.device = device
 
-    def reconstruct(self, video_frame_list: List[PILImage.Image], init_extrinsics: np.ndarray, intrinsics: np.ndarray, cam_pose_list: List[np.ndarray], depth_frame_list: List[np.ndarray] = None) -> Dict[str, np.ndarray]:
+    def reconstruct(self, video_frame_list: List[PILImage.Image], init_extrinsics: np.ndarray, intrinsics: np.ndarray, cam_pose_list: np.ndarray, depth_frame_list: List[np.ndarray] = None) -> Dict[str, np.ndarray]:
         w = video_frame_list[0].width
         fov_x = 2 * np.arctan(w / (2 * intrinsics[0, 0]))
         point_map_list = []
@@ -78,7 +88,12 @@ class MoGeReconstruction(BaseReconstruction):
             points_map = (cam_pose @ points_map_homogeneous.reshape(-1, 4).T).T[:, :3].reshape(points_map.shape)
             point_map_list.append(points_map)
             valid_mask_list.append(valid_mask)
-        return {"rgb": video_frame_list, "intrinsics": intrinsics, "extrinsics": cam_pose_list, "depth": depth_frame_list, "points": point_map_list, "points_mask": valid_mask_list}
+        return {"rgb": video_frame_list, 
+                "intrinsics": intrinsics, 
+                "extrinsics": cam_pose_list, 
+                "depth": depth_frame_list, 
+                "points": point_map_list, 
+                "points_mask": valid_mask_list}
     
 
 class SpatrackerReconstruction(BaseReconstruction):
@@ -88,7 +103,7 @@ class SpatrackerReconstruction(BaseReconstruction):
         self.model = self.model.to(device)
         self.device = device
 
-    def reconstruct(self, video_frame_list: List[PILImage.Image], init_extrinsics: np.ndarray, intrinsics: np.ndarray = None, cam_pose_list: List[np.ndarray] = None, depth_frame_list: List[np.ndarray] = None) -> Dict[str, np.ndarray]:
+    def reconstruct(self, video_frame_list: List[PILImage.Image], init_extrinsics: np.ndarray, intrinsics: np.ndarray = None, cam_pose_list: np.ndarray = None, depth_frame_list: List[np.ndarray] = None) -> Dict[str, np.ndarray]:
         video_tensor_list = []
         for video_frame in video_frame_list:
             video_tensor = pil_to_tensor(video_frame).to(torch.float32).to(self.device)
@@ -132,7 +147,12 @@ class SpatrackerReconstruction(BaseReconstruction):
             points_map_homogeneous = np.concatenate([points_map, ones], axis=-1)
             points_map = (cam_pose @ points_map_homogeneous.reshape(-1, 4).T).T[:, :3].reshape(points_map.shape)
             point_map_list.append(points_map)
-        return {"rgb": video_frame_list, "intrinsics": intrs[0], "extrinsics": extrs, "depth": depth_tensor, "points": np.stack(point_map_list), "points_mask": unc_metric}
+        return {"rgb": video_frame_list, 
+                "intrinsics": intrs[0], 
+                "extrinsics": extrs, 
+                "depth": depth_tensor, 
+                "points": np.stack(point_map_list), 
+                "points_mask": unc_metric}
     
 
 class ViPEReconstruction(BaseReconstruction):
@@ -176,7 +196,11 @@ class ViPEReconstruction(BaseReconstruction):
         sample_depth_frame_list = [depth_frame_list[i] for i in sample_indices]
         sample_point_map_list = [point_map_list[i] for i in sample_indices]
         sample_depth_mask_list = [depth_mask_list[i] for i in sample_indices]
-        return {"intrinsics": intrinsics, "extrinsics": np.stack(sample_cam_pose_list), "depth": np.stack(sample_depth_frame_list), "points": np.stack(sample_point_map_list), "points_mask": np.stack(sample_depth_mask_list)}
+        return {"intrinsics": intrinsics, 
+                "extrinsics": np.stack(sample_cam_pose_list), 
+                "depth": np.stack(sample_depth_frame_list), 
+                "points": np.stack(sample_point_map_list), 
+                "points_mask": np.stack(sample_depth_mask_list)}
     
 
 class DA3DReconstruction(BaseReconstruction):
@@ -218,7 +242,7 @@ class DA3DReconstruction(BaseReconstruction):
         conf_thresh = min(max(conf_thresh, lower), upper)
         return conf_thresh
     
-    def reconstruct(self, video_frame_list: List[PILImage.Image], init_extrinsics: np.ndarray, intrinsics: np.ndarray = None, cam_pose_list: List[np.ndarray] = None, depth_frame_list: List[np.ndarray] = None) -> Dict[str, np.ndarray]:
+    def reconstruct(self, video_frame_list: List[PILImage.Image], init_extrinsics: np.ndarray, intrinsics: np.ndarray = None, cam_pose_list: np.ndarray = None, depth_frame_list: List[np.ndarray] = None) -> Dict[str, np.ndarray]:
         if intrinsics is not None and cam_pose_list is not None and depth_frame_list is not None:
             naive_recon = NaiveReconstruction()
             return naive_recon.reconstruct(video_frame_list, init_extrinsics, intrinsics, cam_pose_list, depth_frame_list)
@@ -261,7 +285,102 @@ class DA3DReconstruction(BaseReconstruction):
                 points_map_homogeneous = np.concatenate([points_map, ones], axis=-1)
                 points_map = (cam_pose @ points_map_homogeneous.reshape(-1, 4).T).T[:, :3].reshape(points_map.shape)
                 point_map_list.append(points_map)
-            return {"rgb": video_frame_list, "intrinsics": intrinsics[0], "extrinsics": np.stack(extrinsics), "depth": np.stack(depth_frame_list), "points": np.stack(point_map_list), "points_mask": np.stack(depth_conf_list) > conf_thresh}
+            return {"rgb": video_frame_list, 
+                    "intrinsics": intrinsics[0], 
+                    "extrinsics": np.stack(extrinsics), 
+                    "depth": np.stack(depth_frame_list), 
+                    "points": np.stack(point_map_list), 
+                    "points_mask": np.stack(depth_conf_list) > conf_thresh}
+
+
+class MapAnythingReconstruction(BaseReconstruction):
+    def __init__(self, model_path: str, device: str = "cuda"):
+        self.model = MapAnything.from_pretrained(model_path)
+        self.model = self.model.to(device=device)
+        self.device = device
+
+    def reconstruct(self, video_frame_list: List[PILImage.Image], init_extrinsics: np.ndarray, intrinsics: np.ndarray = None, cam_pose_list: np.ndarray = None, depth_frame_list: List[np.ndarray] = None) -> Dict[str, np.ndarray]:
+        input_views = []
+        # origin_extrinsics_list = []
+        for frame_id in range(len(video_frame_list)):
+            input_view = {
+                "img": video_frame_list[frame_id],
+            }
+            if intrinsics is not None:
+                input_view["intrinsics"] = intrinsics
+            if cam_pose_list is not None:
+                input_view["extrinsics"] = cam_pose_list[frame_id]
+                input_view["is_metric_scale"] = torch.tensor([True], device=self.device)
+            input_views.append(input_view)
+            # origin_extrinsics_list.append(extrinsic)
+        processed_views = preprocess_inputs(input_views, resize_mode="square", size=700)
+        # Run inference with any combination of inputs
+        outputs = self.model.infer(
+            processed_views,                  # Any combination of input views
+            memory_efficient_inference=False, # Trades off speed for more views (up to 2000 views on 140 GB)
+            use_amp=True,                     # Use mixed precision inference (recommended)
+            amp_dtype="bf16",                 # bf16 inference (recommended; falls back to fp16 if bf16 not supported)
+            apply_mask=True,                  # Apply masking to dense geometry outputs
+            mask_edges=True,                  # Remove edge artifacts by using normals and depth
+            apply_confidence_mask=False,      # Filter low-confidence regions
+            confidence_percentile=5,         # Remove bottom 5 percentile confidence pixels
+            # Control which inputs to use/ignore
+            # By default, all inputs are used when provided
+            # If is_metric_scale flag is not provided, all inputs are assumed to be in metric scale
+            ignore_calibration_inputs=False,
+            ignore_depth_inputs=False,
+            ignore_pose_inputs=False,
+            ignore_depth_scale_inputs=False,
+            ignore_pose_scale_inputs=False,
+        )
+
+        pred_intrinsics_list = []
+        pred_extrinsics_list = []
+        depth_list = []
+        point_map_list = []
+        point_mask_list = []
+        cam2init = None
+        # Access results for each view - Complete list of metric outputs
+        for i, pred in enumerate(outputs):
+            # Geometry outputs
+            pts3d = pred["pts3d"]                     # 3D points in world coordinates (B, H, W, 3)
+            # pts3d_cam = pred["pts3d_cam"]             # 3D points in camera coordinates (B, H, W, 3)
+            depth_z = pred["depth_z"]                 # Z-depth in camera frame (B, H, W, 1)
+            depth_list.append(depth_z[0, :, :, 0].cpu().numpy())
+            # depth_along_ray = pred["depth_along_ray"] # Depth along ray in camera frame (B, H, W, 1)
+
+            # Camera outputs
+            # ray_directions = pred["ray_directions"]   # Ray directions in camera frame (B, H, W, 3)
+            pred_intrinsics = pred["intrinsics"]           # Recovered pinhole camera intrinsics (B, 3, 3)
+            pred_intrinsics_list.append(pred_intrinsics[0].cpu().numpy())
+            pred_camera_poses = pred["camera_poses"]       # OpenCV (+X - Right, +Y - Down, +Z - Forward) cam2world poses in world frame (B, 4, 4)
+            if cam2init is None:
+                cam2init = init_extrinsics @ pred_camera_poses[0].cpu().numpy()
+            pred_extrinsics = cam2init @ np.linalg.inv(pred_camera_poses[0].cpu().numpy())
+            pred_extrinsics_list.append(pred_extrinsics)
+            pts3d = cam2init @ np.concatenate([pts3d.reshape(-1, 3), np.ones((pts3d.shape[0]*pts3d.shape[1], 1))], axis=-1).T
+            pts3d = pts3d.T[:, :3].reshape(pts3d.shape)
+            point_map_list.append(pts3d)
+            # cam_trans = pred["cam_trans"]             # OpenCV (+X - Right, +Y - Down, +Z - Forward) cam2world translation in world frame (B, 3)
+            # cam_quats = pred["cam_quats"]             # OpenCV (+X - Right, +Y - Down, +Z - Forward) cam2world quaternion in world frame (B, 4)
+
+            # Quality and masking
+            confidence = pred["conf"]                 # Per-pixel confidence scores (B, H, W)
+            mask = pred["mask"]                       # Combined validity mask (B, H, W, 1)
+            non_ambiguous_mask = pred["non_ambiguous_mask"]                # Non-ambiguous regions (B, H, W)
+            non_ambiguous_mask_logits = pred["non_ambiguous_mask_logits"]  # Mask logits (B, H, W)
+            point_mask_list.append(mask[0, :, :, 0].cpu().numpy())
+            # Scaling
+            metric_scaling_factor = pred["metric_scaling_factor"]  # Applied metric scaling (B,)
+
+            # Original input
+            img_no_norm = pred["img_no_norm"]         # Denormalized input images for visualization (B, H, W, 3)
+        return {"rgb": video_frame_list, 
+                "intrinsics": pred_intrinsics_list[0] if intrinsics is None else intrinsics, 
+                "extrinsics": np.stack(pred_extrinsics_list) if cam_pose_list is None else cam_pose_list, 
+                "depth": np.stack(depth_list), 
+                "points": np.stack(point_map_list), 
+                "points_mask": np.stack(point_mask_list)}
 
 
 def build_reconstruction_model(input_modality: str, recon_method: str, model_path: str = None, device: str = "cuda") -> BaseReconstruction:
@@ -277,6 +396,8 @@ def build_reconstruction_model(input_modality: str, recon_method: str, model_pat
         recon_model = ViPEReconstruction()
     elif recon_method == "da3":
         recon_model = DA3DReconstruction(model_path=model_path, device=device)
+    elif recon_method == "mapanything":
+        recon_model = MapAnythingReconstruction(model_path=model_path, device=device)
     else:
         raise NotImplementedError(f"Reconstruction method {recon_method} not implemented.")
     return recon_model
